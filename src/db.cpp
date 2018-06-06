40fe8e508ff07f33c4b53144c81d5a005a50206c
collapsedev <4467138+collapsedev@users.noreply.github.com>
2018-06-06 22:34:04 +0200

    Slow start workaround for HDDs
    Speed up start in HDDs with OS cache. Avoid database recursive disk track seek.

--- src/db.cpp
+++ src/db.cpp
@@ -75,6 +75,24 @@ bool CDBEnv::Open(boost::filesystem::path pathEnv_)
     filesystem::path pathErrorFile = pathDataDir / "db.log";
     printf("dbenv.open LogDir=%s ErrorFile=%s\n", pathLogDir.string().c_str(), pathErrorFile.string().c_str());
 
+    // LoadBlockIndex: Disk seek is very slow. We will try to load database in OS cache. Only works when (database size) < (memory size).
+    filesystem::path pathtoidxfile = pathDataDir / "blkindex.dat";
+    struct stat st;
+    stat(pathtoidxfile.string().c_str(), &st);
+    printf("Loading database file through DMA %lu bytes\n",st.st_size);
+    int fd = open(pathtoidxfile.string().c_str(), O_RDONLY, 0);
+    if(fd != -1) {
+        void* mmappedData = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+        if (mmappedData == MAP_FAILED) {
+            printf("Database preload fail. Too few memory?\n",st.st_size);
+        }else{
+            int rc = munmap(mmappedData, st.st_size);
+            if(rc != 0) printf("Database preload error on unmap\n",st.st_size);
+        }
+        close(fd);
+    }
+    //
+    
     unsigned int nEnvFlags = 0;
     if (GetBoolArg("-privdb", true))
         nEnvFlags |= DB_PRIVATE;
